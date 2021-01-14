2f651d86682cdb4e08f98106c8e6df4ba08f3d2e
linkserendipity <462710683@qq.com>
2021-01-14 15:24:59 +0800

    sh

--- aligned/HorizontalMaxPool2D.py
+++ aligned/HorizontalMaxPool2D.py
@@ -6,5 +6,15 @@ class HorizontalMaxPool2d(nn.Module):
 
 
     def forward(self, x):
+        # x feature map
+        # pooling height = 1,  pooling width = W
         inp_size = x.size()
         return nn.functional.max_pool2d(input=x,kernel_size= (1, inp_size[3]))
+
+
+if __name__ == "__main__":
+    import torch
+    x = torch.tensor(32,2048,8,4)
+    hp = HorizontalMaxPool2d()
+    y = hp()
+    print(y.shape)
