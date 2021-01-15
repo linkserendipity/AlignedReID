142a9ebdc200ef4da001f91c1f592e4ff02b2f77
linkserendipity <462710683@qq.com>
2021-01-15 16:38:04 +0800

    sh

--- train.sh
+++ train.sh
@@ -1,2 +1,2 @@
-CUDA_VISIBLE_DEVICES=0 nohup python train_alignedreid.py --train_batch=64  --max_epoch=60 &
+CUDA_VISIBLE_DEVICES=0 nohup python train_alignedreid.py --train-batch=64  --max_epoch=60 &
 # CUDA_VISIBLE_DEVICES=0 python train_alignedreid.py --train_batch=64
